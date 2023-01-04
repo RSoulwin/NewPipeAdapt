191741b0f8e68a40cccb58fcc33fe13e2491b1bf
RSoulwin <72640314+RSoulwin@users.noreply.github.com>
2023-01-04 21:42:57 +0530

    revert  New streams notifications #2335

--- app/proguard-rules.pro
+++ app/proguard-rules.pro
@@ -51,6 +51,3 @@
     private void writeObject(java.io.ObjectOutputStream);
     private void readObject(java.io.ObjectInputStream);
 }
-
-# for some reason NotificationModeConfigFragment wasn't kept (only referenced in a preference xml)
--keep class org.schabi.newpipe.settings.notifications.** { *; }
--- app/src/main/java/org/schabi/newpipe/util/NavigationHelper.java
+++ app/src/main/java/org/schabi/newpipe/util/NavigationHelper.java
@@ -670,12 +670,6 @@ public final class NavigationHelper {
         return getOpenIntent(context, url, service.getServiceId(), linkType);
     }
 
-    public static Intent getChannelIntent(final Context context,
-                                          final int serviceId,
-                                          final String url) {
-        return getOpenIntent(context, url, serviceId, StreamingService.LinkType.CHANNEL);
-    }
-
     /**
      * Start an activity to install Kore.
      *
